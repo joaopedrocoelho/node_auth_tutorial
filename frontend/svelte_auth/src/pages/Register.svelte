e2915f04f848bfba2714ca4ecd2a9d4c72cb8c22
Pedro <joaopedrocoelho2@gmail.com>
2022-10-11 19:59:45 +0800

    :sparkles: added register and login page

--- frontend/svelte_auth/src/pages/Register.svelte
+++ frontend/svelte_auth/src/pages/Register.svelte
@@ -1,29 +1,77 @@
+<script>
+    import axios from "axios";
+    import { push } from "svelte-spa-router";
+
+    let first_name = '';
+    let last_name = '';
+    let email = '';
+    let password = '';
+    let password_confirm = '';
+
+    $: submit = async () => {
+        await axios.post('http://localhost:8000/api/register', {
+            first_name,
+            last_name,
+            email,
+            password,
+            password_confirm
+        })
+
+        await push('/login')
+    }
+
+</script>
+
 <main class="form-signin w-100 m-auto">
-    <form>
+    <form on:submit|preventDefault={submit}>
       <h1 class="h3 mb-3 fw-normal">Register</h1>
   
       <div class="form-floating">
-        <input type="text" class="form-control"  placeholder="First Name">
-        <label>First name</label>
+        <input bind:value={first_name} 
+        type="text" 
+        class="form-control" 
+        id="firstName" 
+        placeholder="First Name">
+        <label for="firstName">First name</label>
       </div>
 
       <div class="form-floating">
-        <input type="text" class="form-control"  placeholder="First Name">
-        <label>Last name</label>
+        <input 
+        bind:value={last_name}
+        type="text" 
+        class="form-control"
+        id="lastName"  
+        placeholder="Last Name">
+        <label for="lastName">Last name</label>
       </div>
 
       <div class="form-floating">
-        <input type="email" class="form-control"  placeholder="name@example.com">
-        <label >Email address</label>
+        <input 
+        bind:value={email}
+        type="email" 
+        class="form-control"  
+        placeholder="name@example.com"
+        id="email">
+        <label for="email">Email address</label>
       </div> 
       <div class="form-floating">
-        <input type="password" class="form-control" placeholder="Password">
-        <label >Password</label>
+        <input 
+        bind:value={password}
+        type="password" 
+        class="form-control" 
+        placeholder="Password"
+        id="password">
+        <label for="password">Password</label>
       </div>
 
       <div class="form-floating">
-        <input type="password" class="form-control" placeholder="Confirm Password">
-        <label>Confirm Password</label>
+        <input 
+        bind:value={password_confirm}
+        type="password" 
+        class="form-control" 
+        placeholder="Confirm Password"
+        id="passwordCofirm">
+        <label for="passwordCofirm">Confirm Password</label>
       </div>
   
       <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ createConnection().then(() => {
     app.use(express.json());
     app.use(cookieParser());
     app.use(cors({
-        origin: ['http://localhost:3000'],
+        origin: ['http://localhost:3000','http://127.0.0.1:5173'],
         credentials: true // this will allow cookies to be sent accross domains
     }))
 
